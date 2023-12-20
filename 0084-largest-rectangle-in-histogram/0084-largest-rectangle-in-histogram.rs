3b5782d46e40e8909824b6d07f821ca810c484a3
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2023-12-20 12:27:09 +0100

    Time: 12 ms (58.33%), Space: 4.1 MB (15.48%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.rs
@@ -0,0 +1,33 @@
+impl Solution {
+    pub fn largest_rectangle_area(heights: Vec<i32>) -> i32 {
+        let mut stack: Vec<(i32, i32)> = Vec::new();
+        let mut max = 0;
+
+        for (idx, hgt) in heights.iter().enumerate() {
+            let (mut old_idx, mut old_hgt) = (idx as i32, 0);
+
+            // New height limits area
+            while stack.len() > 0 && &stack[stack.len() - 1].1 > hgt {
+                (old_idx, old_hgt) = stack.pop().unwrap();
+                let area = old_hgt * (idx as i32 - old_idx);
+
+                if area > max {
+                    max = area;
+                }
+            }
+
+            // Add new element to stack, and extend it back as many times as we poped elements i.e. before limited
+            stack.push((old_idx, *hgt));
+        }
+
+        // Calculate area of remaining elements on stack, that extend all way back
+        for (idx, hgt) in stack.iter() {
+            let area = hgt * (heights.len() as i32 - idx);
+            if area > max {
+                max = area;
+            }
+        }
+
+        max
+    }
+}
