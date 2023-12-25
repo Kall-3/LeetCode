655ff4a82a6119b5c09d75c8f913dbd3ab4fc350
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2023-12-25 12:30:22 +0100

    Time: 0 ms (100.00%), Space: 2.2 MB (16.87%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.rs
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.rs
@@ -6,13 +6,15 @@ impl Solution {
         let mut res = nums[0];
 
         while l <= r {
-            if nums[l - 1] < nums[r - 1] {
-                return res.min(nums[l - 1]);
+            let nums_l = nums[l - 1];
+            if nums_l < nums[r - 1] {
+                return res.min(nums_l);
             }
 
             let m = (l + r) / 2;
-            res = res.min(nums[m - 1]);
-            match nums[m - 1].cmp(&nums[l - 1]) {
+            let nums_m = nums[m - 1];
+            res = res.min(nums_m);
+            match nums_m.cmp(&nums_l) {
                 Greater | Equal => l = m + 1,
                 Less            => r = m - 1,
             }
