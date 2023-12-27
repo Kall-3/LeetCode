3ecc62c326015913c0b08f7cfe8bd17bda565eaa
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2023-12-27 11:16:52 +0100

    Time: 11 ms (50.75%), Space: 3.1 MB (6.79%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let mut lowest_price = i32::MAX;
+        let mut highest_profit = 0;
+
+        for price in &prices {
+            if price < &lowest_price {
+                lowest_price = *price;
+            } else if price - &lowest_price > highest_profit {
+                highest_profit = *price - lowest_price;
+            }
+        }
+
+        highest_profit
+    }
+}
