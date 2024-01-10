91b827eed0744427c843671c3d5c50c83fc3f1f2
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2024-01-10 15:09:30 +0100

    Time: 0 ms (100.00%), Space: 2.3 MB (22.10%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.rs
@@ -0,0 +1,37 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution
+{
+    pub fn is_same_tree(p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> bool
+    {
+        Solution::is_same_tree_rec(p, q)
+    }
+
+    fn is_same_tree_rec(p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> bool
+    {
+        if let (Some(p_node), Some(q_node)) = (p.as_ref(), q.as_ref())
+        {
+                p_node.as_ref().borrow().val == q_node.as_ref().borrow().val
+            && 
+                Solution::is_same_tree_rec
+                (
+                    p_node.as_ref().borrow().left.clone(),
+                    q_node.as_ref().borrow().left.clone()
+                )
+            &&
+                Solution::is_same_tree_rec
+                (
+                    p_node.as_ref().borrow().right.clone(),
+                    q_node.as_ref().borrow().right.clone()
+                )
+        }
+        else if p.as_ref().is_none() && q.as_ref().is_none()
+        {
+            true
+        }
+        else
+        {
+            false
+        }
+    }
+}
