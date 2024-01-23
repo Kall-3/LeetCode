f56fe67bbbd198af2cfb357308d9801a58223999
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2024-01-23 19:38:33 +0100

    Time: 3 ms (88.89%), Space: 2.4 MB (40.74%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.rs
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use std::rc::Rc;
 use std::cell::RefCell;
 
+
 impl Solution
 {
     pub fn is_subtree(root: Option<Rc<RefCell<TreeNode>>>, sub_root: Option<Rc<RefCell<TreeNode>>>)
