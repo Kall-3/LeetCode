f3c60b7b06c8e121ddd792fa0fc6582fb4808a07
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2024-07-05 21:32:43 +0200

    Time: 25 ms (89.24%), Space: 3.9 MB (74.1%) - LeetHub

--- 0015-3sum/0015-3sum.rs
+++ 0015-3sum/0015-3sum.rs
@@ -1,45 +1,42 @@
 impl Solution {
     pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
-        
-        nums.sort();
+        nums.sort_unstable();
 
-        let (mut L, mut R) = (0, 0);
-        let mut res: Vec<_> = vec![];
+        let (mut R, mut L) = (0, 0);
+        let mut res: Vec<Vec<i32>> = Vec::new();
 
-        for (idx, &num) in nums.iter().enumerate().take(nums.len() - 2) {
-            if idx > 0 && nums[idx] == nums[idx - 1] {
+        for (i, &num) in nums.iter().enumerate().take(nums.len() - 2) {
+            if i > 0 && num == nums[i - 1] {
                 continue;
             }
 
-            L = idx + 1;
+            L = i + 1;
             R = nums.len() - 1;
 
             while L < R {
-                let sum = num + nums[L] + nums[R];
-
+                let sum = nums[L] + nums[R] + num;
                 if sum < 0 {
                     L += 1;
                     continue;
-                }
+                } 
                 if sum > 0 {
                     R -= 1;
                     continue;
                 }
 
-                res.push([num, nums[L], nums[R]].to_vec());
+                res.push(vec![nums[i], nums[L], nums[R]]);
 
-                let L_num = nums[L];
-                let R_num = nums[R];
+                let L_val = nums[L];
+                let R_val = nums[R];
 
-                while L < nums.len() && L_num == nums[L] {
+                while L < R && nums[L] == L_val {
                     L += 1;
                 }
-                while R > 0 && R_num == nums[R] {
+                while R > L && nums[R] == R_val {
                     R -= 1;
                 }
             }
         }
-
         res
     }
 }
