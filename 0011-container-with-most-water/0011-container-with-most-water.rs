69ff8dc2b7afd47ab84a14cb3016dbf33a23df2b
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2024-07-05 22:23:59 +0200

    Time: 4 ms (93.4%), Space: 2.9 MB (48.78%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.rs
+++ 0011-container-with-most-water/0011-container-with-most-water.rs
@@ -1,15 +1,10 @@
-use std::cmp::min;
-
 impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
         let (mut L, mut R) = (0, height.len() - 1);
         let mut max = 0;
 
         while L < R {
-            let area = min(height[L], height[R]) * (R - L) as i32;
-            if area > max {
-                max = area;
-            }
+            max = max.max(height[L].min(height[R]) * (R - L) as i32);
 
             if height[L] > height[R] {
                 R -= 1;
