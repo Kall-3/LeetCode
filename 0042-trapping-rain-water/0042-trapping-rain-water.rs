4cf70964d64d6b0e6fb30efec723f5a8ce1d1386
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2024-07-06 15:00:45 +0200

    Time: 0 ms (100%), Space: 2.1 MB (96.75%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.rs
+++ 0042-trapping-rain-water/0042-trapping-rain-water.rs
@@ -1,29 +1,21 @@
 impl Solution {
     pub fn trap(height: Vec<i32>) -> i32 {
-        let mut res = 0;
-        let (mut L, mut R) = (0, height.len() - 1);
-        let (mut L_max, mut R_max) = (height[L], height[R]);
+        let (mut L, mut R) = (1, height.len().max(2) - 2);
+        let (mut L_max, mut R_max) = (height[0], height[height.len() - 1]);
+        let mut water = 0;
 
-        while L < R {
+        while L <= R {
             if L_max <= R_max {
-                let water = L_max.min(R_max) - height[L];
-
-                if water > 0 {
-                    res += water;
-                }
+                water += (L_max.min(R_max) - height[L]).max(0);
                 L_max = L_max.max(height[L]);
                 L += 1;
             } else {
-                R -= 1;
-                let water = L_max.min(R_max) - height[R];
-
-                if water > 0 {
-                    res += water;
-                }
+                water += (L_max.min(R_max) - height[R]).max(0);
                 R_max = R_max.max(height[R]);
+                R -= 1;
             }
         }
-        
-        res
+
+        water
     }
 }
