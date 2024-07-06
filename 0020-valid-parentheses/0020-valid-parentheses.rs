43a54755be3cbd2c6adfcae641c8e08d1e4bfa1c
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2024-07-06 15:41:29 +0200

    Time: 0 ms (100%), Space: 2.1 MB (92.94%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.rs
+++ 0020-valid-parentheses/0020-valid-parentheses.rs
@@ -1,16 +1,14 @@
 impl Solution {
     pub fn is_valid(s: String) -> bool {
-        let mut stack = Vec::new();
+        let mut stack: Vec<char> = Vec::new();
 
         for c in s.chars() {
-            if "({[".contains(c) {
-                stack.push(c);
-            } else {
-                match stack.last() {
-                    Some(last) if (c == ')' && last == &'(') || (c == '}' && last == &'{') || (c == ']' && last == &'[') => { stack.pop(); },
-                    None => return false,
-                    _ => stack.push(c),
-                };
+            match c {
+                '(' => stack.push(')'),
+                '[' => stack.push(']'),
+                '{' => stack.push('}'),
+                ')'|']'|'}' if stack.pop() != Some(c) => return false,
+                _ => (),
             }
         }
 
