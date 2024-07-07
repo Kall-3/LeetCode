e5bb0ea4ba0ab2532724c93c7a22f9eca774f7db
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2024-07-07 19:52:30 +0200

    Time: 0 ms (100%), Space: 2.2 MB (53.67%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.rs
+++ 0022-generate-parentheses/0022-generate-parentheses.rs
@@ -1,31 +1,23 @@
 impl Solution {
-
     pub fn generate_parenthesis(n: i32) -> Vec<String> {
-        let mut res: Vec<String> = Vec::new();
-
-        sol(1, n*2, 1, 0, "(".to_string(), &mut res);
-
-        res
+        let mut stack: Vec<String> = vec![];
+        Self::rec("(".to_string(), &n, 1, 0, &mut stack);
+        stack
     }
 
-}
-
-pub fn sol(current: i32, size: i32, L: i32, R: i32, curr: String, vec: &mut Vec<String>) {
-
-    if current == size {
-        return vec.push(curr);
+    fn rec(cur: String, size: &i32, L: i32, R: i32, res: &mut Vec<String>) {
+        if L == *size && L == R {
+            res.push(cur.clone());
+            return
+        }
+    
+        if L < *size {
+            Self::rec(cur.clone() + "(", size, L + 1, R, res);
+        }
+    
+        if L > R {
+            Self::rec(cur + ")", size, L, R + 1, res);
+        }
     }
 
-    if current < size && L < size/2 {
-        // let mut next_l = curr.clone();
-        // next_l.push('(');
-        let next_l = format!("{}{}", curr, '(');
-        sol(current + 1, size, L + 1, R, next_l, vec);
-    }
-    if L > R {
-        // let mut next_r = curr.clone();
-        // next_r.push(')');
-        let next_r = format!("{}{}", curr, ')');
-        sol(current + 1, size, L, R + 1, next_r, vec);
-    }
 }
