30da72282022533b11338456b2f3bc12a27092dc
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2024-07-07 11:02:10 +0200

    Time: 3 ms (82.2%), Space: 5.8 MB (69.11%) - LeetHub

--- 0155-min-stack/0155-min-stack.rs
+++ 0155-min-stack/0155-min-stack.rs
@@ -3,35 +3,47 @@ struct MinStack {
     min_stack:  Vec<i32>,
 }
 
+
+/** 
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
 impl MinStack {
 
     fn new() -> Self {
-        Self {
-            stack:      Vec::with_capacity(200),
-            min_stack:  Vec::with_capacity(200),
+        MinStack {
+            stack: vec![],
+            min_stack: vec![],
         }
     }
     
     fn push(&mut self, val: i32) {
         self.stack.push(val);
-        if val <= self.get_min() {
+        if self.get_min() >= val {
             self.min_stack.push(val);
         }
     }
     
     fn pop(&mut self) {
-        if self.top() == self.get_min() {
+        if self.stack.pop() == self.min_stack.last().copied() {
             self.min_stack.pop();
         }
-        self.stack.pop();
-        
     }
     
     fn top(&self) -> i32 {
-        *self.stack.last().unwrap()
+        *self.stack.last().unwrap_or(&0)
     }
     
     fn get_min(&self) -> i32 {
         *self.min_stack.last().unwrap_or(&i32::MAX)
     }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * let obj = MinStack::new();
+ * obj.push(val);
+ * obj.pop();
+ * let ret_3: i32 = obj.top();
+ * let ret_4: i32 = obj.get_min();
+ */
