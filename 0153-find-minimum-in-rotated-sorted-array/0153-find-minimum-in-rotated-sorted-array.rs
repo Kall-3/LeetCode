92f0e180169b5aa697c37491d8213ca6d485f16e
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2024-07-13 15:32:22 +0200

    Time: 0 ms (100%), Space: 2 MB (100%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.rs
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.rs
@@ -1,25 +1,17 @@
-use std::cmp::Ordering::{Greater, Less, Equal};
-
 impl Solution {
     pub fn find_min(nums: Vec<i32>) -> i32 {
-        let (mut l, mut r) = (1, nums.len());
-        let mut res = nums[0];
+        let (mut left, mut right) = (0, nums.len() as i32 - 1);
 
-        while l <= r {
-            let nums_l = nums[l - 1];
-            if nums_l < nums[r - 1] {
-                return res.min(nums_l);
-            }
+        while left < right {
+            let mid = (left + right) / 2;
 
-            let m = (l + r) / 2;
-            let nums_m = nums[m - 1];
-            res = res.min(nums_m);
-            match nums_m.cmp(&nums_l) {
-                Greater | Equal => l = m + 1,
-                Less            => r = m - 1,
+            if nums[right as usize] < nums[mid as usize] {
+                left = mid + 1;
+            } else {
+                right = mid;
             }
         }
-        
-        res
+
+        nums[left as usize]
     }
 }
