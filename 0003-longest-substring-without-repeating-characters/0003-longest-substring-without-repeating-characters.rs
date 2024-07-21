7c5aec7a8532fa345687f443f94a14f6ff7ac5ae
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2024-07-21 18:50:45 +0200

    Time: 0 ms (100%), Space: 2.1 MB (82.44%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.rs
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.rs
@@ -1,19 +1,21 @@
-use std::collections::VecDeque;
+use std::collections::HashMap;
 
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
-        let mut set: VecDeque<char> = VecDeque::with_capacity(128);
-        let mut longest = 0;
+        let mut map: HashMap<char, usize> = HashMap::new();
+        let mut res = 0;
 
-        for c in s.chars() {
-            while set.contains(&c) {
-                set.pop_front();
+        let mut left = 0;
+
+        for (i, c) in s.chars().enumerate() {
+            if map.contains_key(&c) {
+                left = left.max(map.insert(c, i).unwrap() + 1);
+            } else {
+                map.insert(c, i);
             }
-
-            set.push_back(c);
-            longest = longest.max(set.len());
+            res = res.max(i - left + 1);
         }
 
-        longest as i32
+        res.max(s.len() - left) as i32
     }
 }
