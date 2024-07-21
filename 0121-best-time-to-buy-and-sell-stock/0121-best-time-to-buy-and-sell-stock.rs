5d340cd398753d25ec624ba37d0b1c3a78e982c3
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2024-07-21 16:58:42 +0200

    Time: 11 ms (51.21%), Space: 3 MB (46%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.rs
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.rs
@@ -1,12 +1,16 @@
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
-        let mut lowest_price = i32::MAX;
-        let mut highest_profit = 0;
+        use std::cmp::min;
+        use std::cmp::max;
+
+        let mut buy = prices[0];
+        let mut profit = 0;
 
         for price in prices {
-            lowest_price = lowest_price.min(price);
-            highest_profit = highest_profit.max(price - lowest_price);
+            profit = max(profit, price - buy);
+            buy = min(buy, price);
         }
-        highest_profit
+
+        profit
     }
 }
