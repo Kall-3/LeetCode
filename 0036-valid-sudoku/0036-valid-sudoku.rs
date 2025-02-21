fc87439cacef75ab0328a55214effc73baa7c5f1
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2025-02-21 13:24:43 +0100

    Time: 0 ms (100%), Space: 2.3 MB (93.08%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.rs
+++ 0036-valid-sudoku/0036-valid-sudoku.rs
@@ -1,44 +1,31 @@
 use std::collections::HashSet;
-use std::any::type_name;
 
 impl Solution {
     pub fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
-        for row in 0..9 {
-            for col in 0..9 {       // Check if a row contains dup
-                if board[row].iter().enumerate().any(|(idx, elem)| elem != &'.' && idx != col && elem == &board[row][col])
-                {
-                    return false;
-                }
-           }
-        }
-        for col in 0..9 {
-            for row in 0..9 {
-                if board.iter().map(|x| x[col]).enumerate().any(|(idx, elem)| elem != '.' && idx != row && &elem == &board[row][col])
-                {
-                    return false;
-                }
+        let mut valid = true;
 
-            }
-        }
         for row in 0..9 {
-            let b_r = row / 3;
-            for col in 0..9 {
-                let b_c = col / 3;
-                // println!("box: {}. b_r, b_c: ({}, {})", boxes, b_r, b_c);
-                if board.iter().enumerate()
-                    .skip(b_r * 3).take(3)
-                    .flat_map(|(r, v)| v.iter()
-                        .enumerate()
-                        .skip(b_c * 3).take(3)
-                        .filter(move |&(c, _)| r != row || c != col)
-                        .map(move |(c, &elem)| (r, c, elem)))
-                    .any(|(r, c, elem)| elem != '.' && &elem == &board[row][col])
-                {
-                    return false;
-                }
+            let row_vec: Vec<&char> = board[row].iter().filter(|x| **x != '.').collect();
+            let row_set: HashSet<&&char> = row_vec.iter().collect();
+            valid &= row_vec.len() == row_set.len();
+        }
+
+        for col in 0..9 {
+            let col_vec: Vec<char> = board.iter().map(|x| x[col]).filter(|x| *x != '.').collect();
+            let col_set: HashSet<&char> = col_vec.iter().collect();
+            valid &= col_vec.len() == col_set.len();
+        }
+
+        for row in 0..3 {
+            for col in 0..3 {
+                let box_vec: Vec<&char> = board.iter().skip(row * 3).take(3).map(|x| {
+                    x.iter().skip(col * 3).take(3)
+                }).flatten().filter(|x| **x != '.').collect();
+                let box_set: HashSet<&&char> = box_vec.iter().collect();
+                valid &= box_vec.len() == box_set.len();
             }
         }
-        
-        return true;
+
+        valid
     }
 }
