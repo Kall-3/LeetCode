36c970edd61dcd4ff233953a5a86c29505750acc
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2025-02-21 10:44:16 +0100

    Time: 0 ms (100%), Space: 3.5 MB (83.42%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.rs
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.rs
@@ -1,22 +1,28 @@
 impl Solution {
     pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
-        let mut prefix: Vec<_> = nums.iter().scan(1, |state, x| {
-            let val = *state;
-            *state *= x;
-            Some(val)
-        }).collect();
-        
-        nums.iter()
-            .enumerate()
-            .rev()
-            .scan(1, |state, (i, x)| {
-                let val = *state;
-                *state *= x;
-                Some((i, val))
-        }).for_each(|(i, postfix)| {
-            prefix[i] *= postfix;
-        });
-        
-        prefix
+        let mut sum = 1;
+        let mut contains_zero_index = vec![];
+
+        for (i, num) in nums.iter().enumerate() {
+            if num != &0 {
+                sum *= num;
+            } else {
+                contains_zero_index.push(i);
+            }
+        }
+
+        if contains_zero_index.is_empty() {
+            let mut ans = vec![sum; nums.len()];
+            for (i, num) in nums.into_iter().enumerate() {
+                ans[i] /= num;
+            }
+            return ans;
+        } else if contains_zero_index.len() > 1 {
+            return vec![0; nums.len()];
+        } else {
+            let mut ans = vec![0; nums.len()];
+            ans[contains_zero_index[0]] = sum;
+            return ans;
+        }
     }
 }
