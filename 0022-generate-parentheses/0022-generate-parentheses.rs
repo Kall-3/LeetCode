43440a8158bd96c496f957de110a8ab3d8242ce3
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2025-02-25 11:08:36 +0100

    Time: 0 ms (100%), Space: 2.4 MB (94.85%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.rs
+++ 0022-generate-parentheses/0022-generate-parentheses.rs
@@ -1,23 +1,23 @@
 impl Solution {
     pub fn generate_parenthesis(n: i32) -> Vec<String> {
-        let mut stack: Vec<String> = vec![];
-        Self::rec("(".to_string(), &n, 1, 0, &mut stack);
-        stack
+        let mut ans: Vec<String> = Vec::new();
+
+        Solution::rec("".to_string(), n, 0, 0, &mut ans);
+
+        ans
     }
 
-    fn rec(cur: String, size: &i32, L: i32, R: i32, res: &mut Vec<String>) {
-        if L == *size && L == R {
-            res.push(cur.clone());
-            return
+    fn rec(curr: String, n: i32, l: i32, r: i32, ans: &mut Vec<String>) {
+        if l + r >= n * 2 {
+            ans.push(curr);
+            return;
         }
-    
-        if L < *size {
-            Self::rec(cur.clone() + "(", size, L + 1, R, res);
+
+        if l > r {
+            Solution::rec(curr.clone() + ")", n, l, r + 1, ans);
         }
-    
-        if L > R {
-            Self::rec(cur + ")", size, L, R + 1, res);
+        if l < n {
+            Solution::rec(curr.clone() + "(", n, l + 1, r, ans);
         }
     }
-
 }
