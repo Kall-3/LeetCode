9d6a0a6fdafc6e6f0fb68eaf1aadc98150a24e04
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2025-02-28 11:24:57 +0100

    Time: 15 ms (95.52%), Space: 4.2 MB (63.96%) - LeetHub

--- 0015-3sum/0015-3sum.rs
+++ 0015-3sum/0015-3sum.rs
@@ -2,41 +2,40 @@ impl Solution {
     pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
         nums.sort_unstable();
 
-        let (mut R, mut L) = (0, 0);
-        let mut res: Vec<Vec<i32>> = Vec::new();
+        let mut solutions: Vec<Vec<i32>> = Vec::new();
 
-        for (i, &num) in nums.iter().enumerate().take(nums.len() - 2) {
-            if i > 0 && num == nums[i - 1] {
+        let (mut left, mut right) = (0, nums.len() - 1);
+
+        for (i, target) in nums.iter().take(nums.len() - 2).enumerate() {
+            if i > 0 && nums[i] == nums[i - 1] {
                 continue;
             }
 
-            L = i + 1;
-            R = nums.len() - 1;
+            left = i + 1;
+            right = nums.len() - 1;
 
-            while L < R {
-                let sum = nums[L] + nums[R] + num;
+            while left < right {
+                let sum = target + nums[left] + nums[right];
+
                 if sum < 0 {
-                    L += 1;
-                    continue;
-                } 
-                if sum > 0 {
-                    R -= 1;
-                    continue;
-                }
+                    left += 1;
+                } else if sum > 0 {
+                    right -= 1;
+                } else {
+                    solutions.push(vec![*target, nums[left], nums[right]]);
+                    left += 1;
+                    right -= 1;
 
-                res.push(vec![nums[i], nums[L], nums[R]]);
-
-                let L_val = nums[L];
-                let R_val = nums[R];
-
-                while L < R && nums[L] == L_val {
-                    L += 1;
-                }
-                while R > L && nums[R] == R_val {
-                    R -= 1;
+                    while left < right && nums[left] == nums[left - 1] {
+                        left += 1;
+                    }
+                    while left < right && nums[right] == nums[right + 1] {
+                        right -= 1;
+                    }
                 }
             }
         }
-        res
+
+        solutions
     }
 }
