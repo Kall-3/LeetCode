7ba18d5e77f89f7e566f1d6301e5199b9e6068fd
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2025-02-28 13:55:50 +0100

    Time: 0 ms (100%), Space: 3.2 MB (34.24%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.rs
+++ 0011-container-with-most-water/0011-container-with-most-water.rs
@@ -1,18 +1,21 @@
 impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
-        let (mut L, mut R) = (0, height.len() - 1);
-        let mut max = 0;
+        let (mut left, mut right) = (0, height.len() - 1);
 
-        while L < R {
-            max = max.max(height[L].min(height[R]) * (R - L) as i32);
+        let mut max_water = 0;
 
-            if height[L] > height[R] {
-                R -= 1;
+        while left < right {
+            let water = (right - left) as i32 * height[left].min(height[right]);
+
+            max_water = max_water.max(water);
+
+            if height[left] < height[right] {
+                left += 1;
             } else {
-                L += 1;
+                right -= 1;
             }
         }
 
-        max
+        max_water
     }
 }
