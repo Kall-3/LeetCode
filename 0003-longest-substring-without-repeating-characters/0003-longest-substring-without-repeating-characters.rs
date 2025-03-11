141ad6a8df656965b2b492c96b992f1d5045eb23
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2025-03-11 11:20:41 +0100

    Time: 0 ms (100%), Space: 2.5 MB (17.25%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.rs
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.rs
@@ -2,20 +2,23 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
+
         let mut map: HashMap<char, usize> = HashMap::new();
-        let mut res = 0;
+        let s: Vec<char> = s.chars().collect();
 
         let mut left = 0;
+        let mut max = 0;
 
-        for (i, c) in s.chars().enumerate() {
-            if map.contains_key(&c) {
-                left = left.max(map.insert(c, i).unwrap() + 1);
-            } else {
-                map.insert(c, i);
+        for (i, c) in s.into_iter().enumerate() {
+            //println!("({}, {})", i, c);
+            if let Some(j) = map.insert(c, i) {
+                left = left.max(j + 1);
+                //println!("new left: {}", left);
             }
-            res = res.max(i - left + 1);
+            //println!("max = {}, i - left = {}", max, i - left);
+            max = max.max(i + 1 - left);
         }
 
-        res as i32
+        max as i32
     }
 }
