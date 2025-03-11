ac267570b59907c6327a6e430eb1117d853fe6b4
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2025-03-11 11:21:32 +0100

    Time: 3 ms (45.62%), Space: 2.4 MB (17.25%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.rs
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.rs
@@ -4,16 +4,15 @@ impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
 
         let mut map: HashMap<char, usize> = HashMap::new();
-        let s: Vec<char> = s.chars().collect();
 
         let mut left = 0;
         let mut max = 0;
 
-        for (i, c) in s.into_iter().enumerate() {
+        for (i, c) in s.chars().enumerate() {
             if let Some(j) = map.insert(c, i) {
                 left = left.max(j + 1);
             }
-            
+
             max = max.max(i + 1 - left);
         }
 
