51aedc5615c5ec70e2649b7758f57379c4aa22bd
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2025-04-07 10:34:08 +0200

    Time: 0 ms (100%), Space: 3.3 MB (73.68%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.rs
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.rs
@@ -1,20 +1,24 @@
+use std::collections::HashSet;
+
 impl Solution {
     pub fn find_duplicate(nums: Vec<i32>) -> i32 {
-        let (mut slow, mut fast) = (nums[0], nums[nums[0] as usize]);
+        let mut slow = 0;
+        let mut fast = 0;
+        loop {
+            slow = nums[slow] as usize;
+            fast = nums[nums[fast] as usize] as usize;
 
-        while slow != fast {
-            slow = nums[slow as usize];
-            fast = nums[nums[fast as usize] as usize];
+            if slow == fast {
+                break;
+            }
         }
 
-        slow = 0;
-
-        while slow != fast {
-            slow = nums[slow as usize];
-            fast = nums[fast as usize];
+        fast = 0;
+        while fast != slow {
+            slow = nums[slow] as usize;
+            fast = nums[fast] as usize;
         }
 
-        
-        slow
+        slow as i32
     }
 }
