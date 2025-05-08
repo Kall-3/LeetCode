bfb98ab3cb5c8117b6c0921c945d40c3434db670
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2025-05-08 20:37:46 +0200

    Time: 0 ms (100%), Space: 2.1 MB (73.84%) - LeetHub

--- 0100-same-tree/0100-same-tree.rs
+++ 0100-same-tree/0100-same-tree.rs
@@ -1,38 +1,42 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
 use std::rc::Rc;
 use std::cell::RefCell;
+impl Solution {
+    pub fn is_same_tree(p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        if let Some(p) = p {
+            if let Some(q) = q {
+                let mut p = p.borrow_mut();
+                let mut q = q.borrow_mut();
 
-impl Solution
-{
-    pub fn is_same_tree(p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> bool
-    {
-        Solution::is_same_tree_rec(p, q)
-    }
+                if p.val != q.val {
+                    return false;
+                }
 
-    fn is_same_tree_rec(p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> bool
-    {
-        if let (Some(p_node), Some(q_node)) = (p.as_ref(), q.as_ref())
-        {
-                p_node.as_ref().borrow().val == q_node.as_ref().borrow().val
-            && 
-                Solution::is_same_tree_rec
-                (
-                    p_node.as_ref().borrow().left.clone(),
-                    q_node.as_ref().borrow().left.clone()
-                )
-            &&
-                Solution::is_same_tree_rec
-                (
-                    p_node.as_ref().borrow().right.clone(),
-                    q_node.as_ref().borrow().right.clone()
-                )
+                return Self::is_same_tree(p.left.take(), q.left.take())
+                    && Self::is_same_tree(p.right.take(), q.right.take());
+            } else {
+                return false;
+            }
+        } else if !q.is_none() {
+            return false;
         }
-        else if p.as_ref().is_none() && q.as_ref().is_none()
-        {
-            true
-        }
-        else
-        {
-            false
-        }
+        true
     }
 }
