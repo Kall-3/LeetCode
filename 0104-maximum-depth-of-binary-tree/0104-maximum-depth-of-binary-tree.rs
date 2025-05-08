09b923eb8235984327cc31bc600d97799c24efc1
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2025-05-08 17:16:22 +0200

    Time: 0 ms (100%), Space: 2.7 MB (75%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.rs
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.rs
@@ -1,20 +1,30 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
 use std::rc::Rc;
 use std::cell::RefCell;
+impl Solution {
+    pub fn max_depth(mut root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        if let Some(node_rc) = root {
+            let mut node = node_rc.borrow_mut();
 
-
-impl Solution
-{
-    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32
-    {
-        if let Some(ref node) = root
-        {
-            let mut ref_node    = node.borrow_mut();
-            let right           = Solution::max_depth(ref_node.right.take()) + 1;
-            let left            = Solution::max_depth(ref_node.left.take()) + 1;
-            right.max(left)
-        }
-        else
-        {
+            1 + Self::max_depth(node.left.take()).max(Self::max_depth(node.right.take()))
+        } else {
             0
         }
     }
