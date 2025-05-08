b770768cb105ba1a5d0d595ee1b4a1b3b8fad32b
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2025-05-08 21:44:21 +0200

    Time: 0 ms (100%), Space: 2.3 MB (88.89%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.rs
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.rs
@@ -20,38 +20,18 @@ use std::rc::Rc;
 use std::cell::RefCell;
 impl Solution {
     pub fn is_subtree(root: Option<Rc<RefCell<TreeNode>>>, sub_root: Option<Rc<RefCell<TreeNode>>>) -> bool {
-        Solution::ist1(root, sub_root.as_ref())
+        Solution::is(root.as_ref(), sub_root.as_ref())
     }
 
-    fn ist1(root: Option<Rc<RefCell<TreeNode>>>, sub_root: Option<&Rc<RefCell<TreeNode>>>) -> bool {
-        if Solution::ist2(root.as_ref(), sub_root) {
-            true
-        } else {
-            if let Some(root) = root {
-                let mut root = root.borrow_mut();
-
-                Solution::ist1(root.left.take(), sub_root) || Solution::ist1(root.right.take(), sub_root)
-            } else {
-                false
-            }
+    fn is(root: Option<&Rc<RefCell<TreeNode>>>, sub_root: Option<&Rc<RefCell<TreeNode>>>) -> bool {
+        if root == sub_root {
+            return true;
         }
-    }
+        if let Some(node) = root {
+            let mut node = node.borrow_mut();
 
-    fn ist2(root: Option<&Rc<RefCell<TreeNode>>>, sub_root: Option<&Rc<RefCell<TreeNode>>>) -> bool {
-        if let Some(root) = root {
-            if let Some(sub) = sub_root {
-                let sub = sub.borrow();
-                let root = root.borrow();
-
-                if root.val == sub.val {
-                    return Solution::ist2(root.left.as_ref(), sub.left.as_ref())
-                        && Solution::ist2(root.right.as_ref(), sub.right.as_ref());
-                }
-            }
-        }
-
-        if root.is_none() && sub_root.is_none() {
-            true
+            Self::is(node.left.take().as_ref(), sub_root) ||
+                Self::is(node.right.take().as_ref(), sub_root)
         } else {
             false
         }
