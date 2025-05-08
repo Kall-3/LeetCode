1e5ee9af4874195a9232731205a90bc93a33f57a
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2025-05-08 21:32:23 +0200

    Time: 0 ms (100%), Space: 2.3 MB (47.89%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.rs
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.rs
@@ -1,114 +1,59 @@
-use std::collections::HashMap;
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
 use std::rc::Rc;
 use std::cell::RefCell;
-
-impl Solution
-{
-    pub fn is_subtree(root: Option<Rc<RefCell<TreeNode>>>, sub_root: Option<Rc<RefCell<TreeNode>>>)
-    -> bool
-    {
-        // Depth of sub-tree
-        let root_depth = Solution::max_depth(sub_root.clone());
-
-        // Create new tree containing max-depths
-        let mut depths = HashMap::new();
-
-        // Calculate max-depths
-        Solution::max_depth_save(root.clone(), &mut depths);
-
-        // Find sub-tree
-        Solution::is_subtree_rec(root, &sub_root, &depths, root_depth)
+impl Solution {
+    pub fn is_subtree(root: Option<Rc<RefCell<TreeNode>>>, sub_root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        Solution::ist1(root, sub_root.as_ref())
     }
 
-    // Find if depth matches, then check sub-tree
-    fn is_subtree_rec
-    (
-        root: Option<Rc<RefCell<TreeNode>>>,
-        sub_root: &Option<Rc<RefCell<TreeNode>>>,
-        depths: &HashMap<*const RefCell<TreeNode>, i32>,
-        root_depth: i32
-    ) -> bool
-    {
-        if let Some(node) = root
-        {
-            if let Some(depth) = depths.get(&Rc::as_ptr(&node))
-            {
-                if depth == &root_depth
-                {
-                    if Solution::check_same(Some(node.clone()), sub_root)
-                    {
-                        return true
-                    }
-                }
+    fn ist1(root: Option<Rc<RefCell<TreeNode>>>, sub_root: Option<&Rc<RefCell<TreeNode>>>) -> bool {
+        if Solution::ist2(root.as_ref(), sub_root) {
+            true
+        } else {
+            if let Some(root) = root {
+                let mut root = root.borrow_mut();
 
-                let temp = Solution::is_subtree_rec(node.borrow_mut().left.take(), &sub_root, &depths, root_depth);
-                if temp { return true }
-                return temp || Solution::is_subtree_rec(node.borrow_mut().right.take(), &sub_root, &depths, root_depth);
+                Solution::ist1(root.left.take(), sub_root) || Solution::ist1(root.right.take(), sub_root)
+            } else {
+                false
             }
         }
-        false
     }
 
-    fn check_same(
-        root: Option<Rc<RefCell<TreeNode>>>,
-        sub_root: &Option<Rc<RefCell<TreeNode>>>,
-    ) -> bool
-    {
-        match (root, sub_root) {
-            (Some(root_node), Some(sub_node)) => {
-                if root_node.borrow().val == sub_node.borrow().val
-                {
-                    return Solution::check_same(root_node.borrow().left.clone(), &sub_node.borrow().left.clone()) && Solution::check_same(root_node.borrow().right.clone(), &sub_node.borrow().right.clone());
+    fn ist2(root: Option<&Rc<RefCell<TreeNode>>>, sub_root: Option<&Rc<RefCell<TreeNode>>>) -> bool {
+        if let Some(root) = root {
+            if let Some(sub) = sub_root {
+                let sub = sub.borrow();
+                let root = root.borrow();
+
+                if root.val == sub.val {
+                    return Solution::ist2(root.left.as_ref(), sub.left.as_ref())
+                        && Solution::ist2(root.right.as_ref(), sub.right.as_ref());
                 }
             }
-            (None, None) => {
-                return true
-            }
-            _ => {
-                return false
-            }
         }
-        false
+
+        if root.is_none() && sub_root.is_none() {
+            true
+        } else {
+            false
+        }
     }
-
-    // Save all depths from all nodes
-    fn max_depth_save
-    (
-        root: Option<Rc<RefCell<TreeNode>>>,
-        depths: &mut HashMap<*const RefCell<TreeNode>, i32>
-    ) -> i32
-    {
-        if let Some(node) = root
-        {
-            // Calculate max-depth
-            let left    = Solution::max_depth_save(node.borrow().left.clone(), depths);
-            let right   = Solution::max_depth_save(node.borrow().right.clone(), depths);
-            let max     = 1 + left.max(right);
-
-            // Save depth
-            // println!("d: {}, v: {}", max, node.borrow().val);
-            depths.insert(Rc::as_ptr(&node), max);
-            max
-        }
-        else
-        {
-            0
-        }
-    } 
-
-    // Find all depths
-    fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32
-    {
-        if let Some(ref node) = root
-        {
-            let mut ref_node    = node.borrow_mut();
-            let right           = Solution::max_depth(ref_node.right.clone()) + 1;
-            let left            = Solution::max_depth(ref_node.left.clone()) + 1;
-            right.max(left)
-        }
-        else
-        {
-            0
-        }
-    } 
 }
