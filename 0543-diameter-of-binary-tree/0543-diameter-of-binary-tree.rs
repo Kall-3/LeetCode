b70b7708ccf09f08a3f33d13c50523b8034728aa
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2025-05-08 19:44:23 +0200

    Time: 0 ms (100%), Space: 3.6 MB (55.56%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.rs
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.rs
@@ -1,25 +1,40 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
 use std::rc::Rc;
 use std::cell::RefCell;
-impl Solution
-{
-    pub fn diameter_of_binary_tree(mut root: Option<Rc<RefCell<TreeNode>>>) -> i32
-    {
-        Solution::diameter_recursive(root).1
+impl Solution {
+    pub fn diameter_of_binary_tree(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let mut max = 0;
+        Solution::dobt(root, &mut max);
+        max
     }
 
-    fn diameter_recursive(mut root: Option<Rc<RefCell<TreeNode>>>) -> (i32, i32)
-    {
-        if let Some(node) = root
-        {
-            let mut ref_node    = node.borrow_mut();
-            let (left_depth, left_dia) = Solution::diameter_recursive(ref_node.left.take());
-            let (right_depth, right_dia) = Solution::diameter_recursive(ref_node.right.take());
+    fn dobt(root: Option<Rc<RefCell<TreeNode>>>, max: &mut i32) -> i32 {
+        if let Some(root) = root {
+            let left = Solution::dobt(root.borrow_mut().left.take(), max);
+            let right = Solution::dobt(root.borrow_mut().right.take(), max);
 
-            (left_depth.max(right_depth) + 1, left_dia.max(right_dia.max(left_depth + right_depth)))
-        }
-        else
-        {
-            (0, 0)
+            *max = *max.max(&mut (left + right));
+
+            1 + left.max(right)
+        } else {
+            0
         }
     }
 }
