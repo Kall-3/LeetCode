3af8fe4ebbf34ce76d720c15fbdc1456483f254e
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2025-05-08 19:58:46 +0200

    Time: 0 ms (100%), Space: 2.8 MB (80%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.rs
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.rs
@@ -1,24 +1,43 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
 use std::rc::Rc;
 use std::cell::RefCell;
+
 impl Solution {
     pub fn is_balanced(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
-        Solution::is_balanced_recursive(root).1
+        Solution::depth(root.as_ref()) != -1
     }
 
-    fn is_balanced_recursive(root: Option<Rc<RefCell<TreeNode>>>) -> (i32, bool)
-    {
-        if let Some(node) = root
-        {
-            let mut ref_node = node.borrow_mut();
+    fn depth(root: Option<&Rc<RefCell<TreeNode>>>) -> i32 {
+        if let Some(node_rc) = root {
+            let left = Solution::depth(node_rc.borrow().left.as_ref());
+            let right = Solution::depth(node_rc.borrow().right.as_ref());
 
-            let left    = Solution::is_balanced_recursive(ref_node.left.take());
-            let right   = Solution::is_balanced_recursive(ref_node.right.take());
-
-            (left.0.max(right.0) + 1, ((left.0 - right.0).abs() < 2 && left.1 && right.1))
-        }
-        else
-        {
-            (0, true)
+            if left == -1 || right == -1 {
+                -1
+            } else if (left - right).abs() < 2 {
+                1 + left.max(right)
+            } else {
+                -1
+            }
+        } else {
+            0
         }
     }
 }
