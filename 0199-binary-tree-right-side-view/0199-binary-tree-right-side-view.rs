8120539ff99d2f8f717eae7be971310fbd36a766
Karl-Johan Petersson <48130159+Kall-3@users.noreply.github.com>
2025-09-16 14:03:33 +0200

    Time: 0 ms (100%), Space: 2.1 MB (71.05%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.rs
@@ -0,0 +1,45 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+
+type Node = Rc<RefCell<TreeNode>>;
+type List = Vec<Vec<i32>>;
+
+impl Solution {
+    pub fn right_side_view(root: Option<Node>) -> Vec<i32> {
+        let mut levels: List = Vec::with_capacity(5);
+        Self::lo(0, &root, &mut levels);
+        levels.shrink_to_fit();
+
+        levels.into_iter()
+            .map(|lvl| *lvl.last().unwrap())
+            .collect::<Vec<i32>>()
+    }
+
+    fn lo(depth: usize, root: &Option<Node>, res: &mut List) {
+        if let Some(root_rc) = root.as_ref() {
+            if let Some(v) = res.get_mut(depth) { v.push(root_rc.borrow().val) }
+            else { res.push(vec![root_rc.borrow().val]) }
+
+            Self::lo(depth + 1, &root_rc.borrow().left, res); 
+            Self::lo(depth + 1, &root_rc.borrow().right, res); 
+        }
+    }
+}
